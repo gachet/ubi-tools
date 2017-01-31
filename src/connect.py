0f8615b826cf9addbd95fd4b38524856218845f1
Jose <jose.reyes@ubidots.com>
2017-01-31 11:12:54 -0500

    added a function for getting data from ubidots

--- src/connect.py
+++ src/connect.py
@@ -4,7 +4,7 @@ import os
 def post_var(token, ubi_source, ubi_var, value, timestamp=None, context=None):
 	
 	'''
-	This function post data to ubidots
+	This function posts data to ubidots
 
 	Input parameters:
 
@@ -18,7 +18,7 @@ def post_var(token, ubi_source, ubi_var, value, timestamp=None, context=None):
 
 	try:
 		url = os.getenv("UBIDOTS_URL") if os.getenv("UBIDOTS_URL") is not None else "http://things.ubidots.com/"
-		url = url + "api/v1.6/devices/" + ubi_source + "/?token=" + token
+		url = url + "api/v1.6/devices/" + ubi_source + "/"
 		headers = {"X-Auth-Token": token,
 					"Content-Type":"application/json"}
 		data = {ubi_var:{"value": value}}
@@ -30,4 +30,29 @@ def post_var(token, ubi_source, ubi_var, value, timestamp=None, context=None):
 	except Exception as e:
 		return e
 
+def get_var(token, ubi_source, ubi_var, value, timestamp=None, context=None):
+	
+	'''
+	This function gets data from ubidots
+
+	Input parameters:
+
+	token: The Ubidots token
+	ubi_source: The name of the user's Ubidots datasource
+	ubi_var: The name of the user's Ubidots variable
+	
+	Return:
+
+	Returns in this order the next parameters: value, timestamp, context
+	'''
+
+	try:
+		url = os.getenv("UBIDOTS_URL") if os.getenv("UBIDOTS_URL") is not None else "http://things.ubidots.com/"
+		url = url + "api/v1.6/devices/" + ubi_source + "/" + ubi_var + "/values?page_size=1"
+ 		headers = {"X-Auth-Token": token,
+					"Content-Type":"application/json"}
+		r = requests.get(url=url, headers=headers)
+		return r.json()[0]['value'], r.json()[0]['timestamp'], r.json()[0]['context']
+	except Exception as e:
+		return e
 
